2b2327ddf085e0b0360df63b7bfe5d6fd8655970
matjazonline <matjazonline@gmail.com>
2023-11-16 15:39:30 +0100

    use account in AccountPill.jsx

--- core/src/components/AccountPill.jsx
+++ core/src/components/AccountPill.jsx
@@ -1,14 +1,14 @@
-import React, { useContext, useState } from 'react'
+import React, {useContext, useState} from 'react'
 import Uik from "@reef-chain/ui-kit";
 import ReefStateContext from '../context/ReefStateContext';
-import { availableNetworks, utils,hooks } from '@reef-chain/react-lib';
-import { reefState } from '@reef-chain/util-lib';
+import {availableNetworks, hooks, utils} from '@reef-chain/react-lib';
 
 
 function AccountPill() {
-  
+
     const [isOpen, setOpen] = useState(false)
-    const { selectedReefSigner,signers } = useContext(ReefStateContext);
+    const { signers, network, reefState } = useContext(ReefStateContext);
+  const selectedAccount = hooks.useObservableState(reefState.selectedAccount$);
 
     const selectNetwork = (key)=> {
       const toSelect = availableNetworks[key];
@@ -19,18 +19,16 @@ function AccountPill() {
     reefState.setSelectedAddress(account.address)
   }
 
-  const selectedNetwork = hooks.useObservableState(reefState.selectedNetwork$);
-
   return (
     <div className='account-pill-btn'>
-    <Uik.Button text={selectedReefSigner.name} rounded fill size='small' onClick={()=>setOpen(true)} className='account-pill-btn-name'/>
-    <Uik.Button text={utils.shortAddress(selectedReefSigner.address)} rounded size='small' onClick={()=>setOpen(true)} className='account-pill-btn-address'/>
+    <Uik.Button text={selectedAccount?.name} rounded fill size='small' onClick={()=>setOpen(true)} className='account-pill-btn-name'/>
+    <Uik.Button text={selectedAccount?utils.shortAddress(selectedAccount?.address):''} rounded size='small' onClick={()=>setOpen(true)} className='account-pill-btn-address'/>
     <Uik.AccountSelector
     isOpen={isOpen}
     accounts={signers}
-    selectedAccount={selectedReefSigner}
+    selectedAccount={selectedAccount}
     onNetworkSelect={selectNetwork}
-    selectedNetwork={selectedNetwork?selectedNetwork.name:availableNetworks.mainnet.name}
+    selectedNetwork={network?network.name:availableNetworks.mainnet.name}
     onClose={() => setOpen(false)}
     onSelect={account => selectAccount(account)}
   />
